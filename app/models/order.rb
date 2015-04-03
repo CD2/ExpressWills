c9ce8906631cfb191d40a3df6d608f263a3c56a7
Shane <shane@cd2solutions.co.uk>
2015-04-03 12:05:42 +0100

    asd

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,7 +11,6 @@ class Order < ActiveRecord::Base
         return: "#{Rails.application.secrets.app_host}#{return_path}",
         invoice: id,
         amount: price,
-        currency: 'GBP',
         item_name: full_name,
         item_number: '1',
         quantity: '1',

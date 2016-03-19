972c0b200d115ed617a90b427337ff1984b54c11
Shane <shane@cd2solutions.co.uk>
2016-03-19 11:43:54 +0000

    this

--- app/controllers/wills_controller.rb
+++ app/controllers/wills_controller.rb
@@ -236,6 +236,7 @@ class WillsController < ApplicationController
   def email
     save_final_will
     WillPurchased.resend_will(@will).deliver
+    flas[:notice] = "Will emailed"
     redirect_to admin_wills_path
   end
 

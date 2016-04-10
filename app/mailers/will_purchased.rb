8c492a589341313222e6d5976ba55d6a061fb67c
Shane <shane@cd2solutions.co.uk>
2016-04-10 12:02:05 +0100

    this

--- app/mailers/will_purchased.rb
+++ app/mailers/will_purchased.rb
@@ -6,22 +6,22 @@ class WillPurchased < ActionMailer::Base
     @will = will
     attachments["WILLSigningInstructions.pdf"] = File.read("#{Rails.root}/public/WILLSigningInstructions.pdf")
     attachments["WILL.pdf"] = File.read("#{Rails.root}/tmp/will_#{@will.id}.pdf")
-    #mail(to: @will.user.email, subject: "Thank you for your purchase")
-    mail(to: "shane@cd2solutions.co.uk", subject: "Thank you for your purchase")
+    mail(to: @will.user.email, subject: "Thank you for your purchase")
+    #mail(to: "shane@cd2solutions.co.uk", subject: "Thank you for your purchase")
   end
 
   def merlin_notify(will)
     @will = will
-    #mail(to: 'contact@notaryexpress.co.uk', subject: "Will Purchased")
-    mail(to: 'shane@cd2solutions.co.uk', subject: "Will Purchased")
+    mail(to: 'contact@notaryexpress.co.uk', subject: "Will Purchased")
+    #mail(to: 'shane@cd2solutions.co.uk', subject: "Will Purchased")
   end
 
   def resend_will(will)
     @will = will
     attachments["WILL.pdf"] = File.read("#{Rails.root}/tmp/will_#{@will.id}.pdf")
     attachments["WILLSigningInstructions.pdf"] = File.read("#{Rails.root}/public/WILLSigningInstructions.pdf")
-    #mail(to: @will.user.email, subject: "Your reviewed will")
-    mail(to: "shane@cd2solutions.co.uk", subject: "Your reviewed will")
+    mail(to: @will.user.email, subject: "Your reviewed will")
+    #mail(to: "shane@cd2solutions.co.uk", subject: "Your reviewed will")
   end
 
 end
